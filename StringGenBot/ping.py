443a1499e257886950a33b9596870b773f719c3b
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-05-15 15:46:44 +0530

    Update ping.py

--- StringGenBot/ping.py
+++ StringGenBot/ping.py
@@ -45,7 +45,7 @@ async def ping_handler(client: Client, message: Message):
 ➥ ᴘᴏɴɢ: `{latency:.3f} ms`
 ➥ ᴜᴘᴛɪᴍᴇ: {uptime}
 
-⦿ ᴄʀᴇᴀᴛᴇᴅ ʙʏ [sʜɪᴠᴀɴsʜ-xᴅ](https://t.me/ITSZ_SHIVANSH)**""",
+⦿ ᴄʀᴇᴀᴛᴇᴅ ʙʏ [ ᴛʜᴀᴋᴜʀ ](https://t.me/itszthakur)**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
