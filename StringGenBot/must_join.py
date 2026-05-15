65c8cbbc69fa93943eed7f1fcdb894183740faef
ᯓ‌𐌌།⃪᱂ ⚠️𝑻𝒉𝒂𝒌𝒖𝒓𓂃 <reetsingh274@gmail.com>
2026-05-16 03:38:09 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -19,7 +19,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://files.catbox.moe/520y6h.jpg", caption=f"**» ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ғᴀᴍɪʟʏ ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ.\n\n➥ ᴊᴏɪɴ [ᴜᴘᴅᴀᴛᴇs 🪧]({link}).\n\n» ᴀғᴛᴇʀ ᴊᴏɪɴ /start ᴍᴇ ᴀɢᴀɪɴ 🌹!**",
+                    photo="https://o.uguu.se/CwiFqBFb.jpg", caption=f"**» ғɪʀsᴛʟʏ ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴏᴜʀ ғᴀᴍɪʟʏ ᴛʜᴇɴ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ.\n\n➥ ᴊᴏɪɴ [ᴜᴘᴅᴀᴛᴇs 🪧]({link}).\n\n» ᴀғᴛᴇʀ ᴊᴏɪɴ /start ᴍᴇ ᴀɢᴀɪɴ 🌹!**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
